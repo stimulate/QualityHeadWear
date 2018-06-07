32252ec45bb5d6d20b5d48e4deb48343ceb79437
Sabrina Yu <a_yu_apple@hotmail.com>
2018-06-07 19:33:37 +1200

    create3

---
+++ database/migrations/2018_06_07_004902_create_caps_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCapsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('caps', function (Blueprint $table) {
+            $table->increments('CapID');
+            $table->string('name',50);
+            $table->string('description');
+            $table->blob('image');
+            $table->double('price');
+            $table->integer('CategoryID')->unsigned()->index(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('caps');
+    }
+}
